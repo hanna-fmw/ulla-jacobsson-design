da94d65159cd359cf189e4ddc5e932b603f63781
Hanna Hosk <hanna.hosk@gmail.com>
2024-06-25 17:04:51 +0200

    Updated styling for HeroContent component

--- app/components/HeroContent.tsx
+++ app/components/HeroContent.tsx
@@ -6,16 +6,14 @@ type Props = {
 }
 
 const HeroContent = ({ postTitle, subheading }: Props) => (
-	<div className='absolute bottom-[10%] w-1/2 md:bottom-[20%] md:w-1/2 lg:w-[35%] px-[20px] md:px-[70px]'>
-		<h2 className='text-navbar-link-hover-color text-base md:text-lg font-semibold uppercase tracking-[1px] '>
+	<div className='absolute bottom-[10%] right-0 w-1/2 md:bottom-[35%] md:w-1/2 lg:w-[60%] px-[20px] md:px-[70px]'>
+		<h2 className='text-base md:text-lg font-semibold uppercase tracking-[1px] '>
 			Ulla Jacobsson Design
 		</h2>
-		<h1 className='text-hero-title-color font-lora overflow-hidden text-5xl sm:text-6xl md:text-6xl'>
+		<h1 className='text-hero-title-color-pink font-lora overflow-hidden text-5xl sm:text-6xl md:text-6xl'>
 			{postTitle}
 		</h1>
-		<p className='text-navbar-link-hover-color font-inter text-base md:text-xl mt-3'>
-			{subheading}
-		</p>
+		<p className='font-inter text-base md:text-xl mt-3'>{subheading}</p>
 	</div>
 )
 
